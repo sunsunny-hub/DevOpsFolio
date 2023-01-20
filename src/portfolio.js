dd4a0303a510caaf7466704837b8c0dd31e89484
sunsunny-hub <surajsingh5233@gmail.com>
2023-01-20 16:14:00 +0530

    Updated in my github repo

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,26 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Suraj Singh Thakur",
+  title: "Hi all, I'm Suraj",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate DevOps Engineer 🚀 having an experience of building complete infrastructure using IaC and CI/CD pipeline using global tools and cloud devops tools."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1sbxGJcOT_fXSw-IcYt37_xMNj1XNrLot/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/sunsunny-hub",
+  linkedin: "www.linkedin.com/in/suraj-singh-thakur-👨‍💻👨‍🔧-0a6836100",
+  gmail: "surajsingh5233@gmail.com",
+//  gitlab: "https://gitlab.com/saadpasta",
+  facebook: "https://www.facebook.com/SURAJSINGH000",
+  medium: "https://medium.com/@surajsingh5233",
+//  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -370,7 +370,7 @@ const contactInfo = {
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "SurajSingh__", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
